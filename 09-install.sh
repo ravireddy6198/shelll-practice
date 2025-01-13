b080b35d7667313561aeaae53d497068a7647821
ravireddy6198 <raviprakash03051992@gmail.com>
2025-01-13 11:44:49 +0530

    ansible

--- 09-install.sh
+++ 09-install.sh
@@ -6,6 +6,12 @@ R="\e[31m"
 G="\2[32m"
 Y="\e[33m"
 
+LOG_FOLDER="/var/log/shell-script"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d---%H:%M:%S)
+LOG_FILE_NAME="$LOG_FOLDER/$LOG_FILE   $TIMESTAMP.log"
+
+
 if [ $USERID -ne 0 ]
 then
     echo "ERROR :: You must have root access to install mysql"
@@ -22,11 +28,11 @@ VALIDATE(){
     fi
 }
 
-apt list --installed
+apt list --installed &>>$LOG_FILE_NAME
 
 if [ $? -ne 0 ]
 then
-    apt-get install mysql-server -y
+    apt-get install mysql-server -y &>>$LOG_FILE_NAME
     VALIDATE $? "Installing Mysql"
     
 else
