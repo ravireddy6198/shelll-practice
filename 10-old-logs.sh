9cd5c8e549ef326a80db9bc297b5d27231ddf93a
ravireddy6198 <raviprakash03051992@gmail.com>
2025-01-13 16:26:53 +0530

    ansible

--- 10-old-logs.sh
+++ 10-old-logs.sh
@@ -8,11 +8,20 @@ echo "files to delete : $FILES_DELETE"
 
 while read -r filepath
 do
-
         echo " deleting file : $filepath"
         rm -rf $filepath
-
-
 done <<< $FILES_DELETE
 
 echo " all files deleted"
+
+
+
+# FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+# echo "Files to be deleted: $FILES_TO_DELETE"
+
+# while read -r filepath # here filepath is the variable name, you can give any name
+# do
+#     echo "Deleting file: $filepath" &>>$LOG_FILE_NAME
+#     rm -rf $filepath
+#     echo "Deleted file: $filepath"
+# done <<< $FILES_TO_DELETE
