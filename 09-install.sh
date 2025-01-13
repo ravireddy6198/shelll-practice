70317b77d1436f4067aa63bf6271e763bc65fd33
ravireddy6198 <raviprakash03051992@gmail.com>
2025-01-13 08:32:20 +0530

    shell-script

--- 09-install.sh
+++ 09-install.sh
@@ -5,4 +5,19 @@ then
     echo "ERROR :: You must have root access to install mysql"
 fi
 
-apt install mysql-server
+apt list installed mysql
+
+if [ $? -ne 0 ]
+then
+    apt install mysql-server -y
+    if [ $? -ne 0 ]
+    then
+        echo " Mysql installing ....... failure"
+    else
+        echo " Mysql installing ....... success"
+    fi
+else
+    echo " Mysql os already ........ installed"
+fi
+
+echo " script continued"
