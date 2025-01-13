69691c8813e6cc1f364cd3249ba77da0fc718067
ravireddy6198 <raviprakash03051992@gmail.com>
2025-01-13 10:53:59 +0530

    ansible

--- 09-install.sh
+++ 09-install.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
-if [ $(id -u) -ne 0 ]
+USERID=$(id -u)
+
+R="\e[31m"
+G="\2[32m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ]
 then
     echo "ERROR :: You must have root access to install mysql"
     exit 1
 fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e " $2 ....... $R failure"
+        exit 1
+    else
+        echo -e " $2 ....... $G success"
+    fi
+}
 
 apt list --installed
 
 if [ $? -ne 0 ]
 then
     apt-get install mysql-server -y
-    if [ $? -ne 0 ]
-    then
-        echo " Mysql installing ....... failure"
-        exit 1
-    else
-        echo " Mysql installing ....... success"
-    fi
+    VALIDATE $? "Installing Mysql"
+    
 else
-    echo " Mysql os already ........ installed"
+     -e " Mysql os already ........$Y installed"
 fi
 
 echo " script continued"
