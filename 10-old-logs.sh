1b1f0516071b31f20ee5f57eed9ea73192cc3af3
ravireddy6198 <raviprakash03051992@gmail.com>
2025-01-13 16:19:41 +0530

    ansible

---
+++ 10-old-logs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+SOURCE="/home/ravi/old-logs"
+
+FILES_DELETE=$(find $SOURCE -name "*.log" -mtime +14)
+
+echo "files to delete : $FILES_DELETE"
+
+while read -r filepath
+do
+
+        echo " deleting file : $filepath
+        rm -rf $filepath
+
+
+done >>>$FILES_DELETE
+
+echo " all files deleted"
