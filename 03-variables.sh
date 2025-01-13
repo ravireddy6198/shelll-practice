ecae5df4579cbc8cac20b23ef945f7a7184a0268
ravireddy6198 <raviprakash03051992@gmail.com>
2025-01-13 05:49:27 +0530

    shell-script

--- 03-variables.sh
+++ 03-variables.sh
@@ -6,4 +6,4 @@ NAME2=$2
 echo "$NAME1 :: Hello $NAME2"
 echo "$NAME2 :: HI How are you $NAME1"
 echo "${NAME1} :: Fine ra $NAME2"
-echo "$NAME2 ::what are you doing $NAME!" 
+echo "$NAME2 ::what are you doing $NAME1" 
---
+++ 04-variables.sh
@@ -0,0 +1,6 @@
+
+echo " Please enter USername "
+read USERNAME
+echo $USERNAME
+echo " Please enter password"
+read PASSWORD
